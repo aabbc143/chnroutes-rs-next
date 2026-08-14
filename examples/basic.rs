a85b1d2235ae4c7ea9d5bda127438d45deed6526
喵Ai <75683404+aabbc143@users.noreply.github.com>
2026-08-14 15:58:38 +0800

    fix: use correct Source::Apnic variant

--- examples/basic.rs
+++ examples/basic.rs
@@ -6,7 +6,7 @@ async fn main() -> Result<()> {
     /// Get the CN IPs from APNIC
     let cn_ip_results: Vec<ipnet::IpNet> = chnroutes::source::apnic::fetch_ip_data()?;
     /// Get the user script
-    let user_script: Result<(String, Option<String>)> = Target::Linux.export_str(&Source::apnic);
+    let user_script: Result<(String, Option<String>)> = Target::Linux.export_str(&Source::Apnic);
     /// Apply rules to system route table
     chnroutes::up(&Default::default()).await?;
     Ok(())
