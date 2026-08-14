f0e6d7c380d0ac1642a383338cd506f88ecd83cf
喵Ai <75683404+aabbc143@users.noreply.github.com>
2026-08-14 14:59:46 +0800

    fix: handle UTF-8 parsing errors

--- src/error.rs
+++ src/error.rs
@@ -1,48 +1,63 @@
 use thiserror::Error;
+
 #[derive(Error, Debug)]
 pub enum Error {
     #[error("Network error: {0}")]
     NetworkError(#[from] reqwest::Error),
+
     #[error("IO error: {0}")]
     IOError(#[from] std::io::Error),
+
+    #[error("UTF-8 error: {0}")]
+    Utf8Error(#[from] std::string::FromUtf8Error),
+
     #[error("Cache error: {0}")]
     CacheError(#[from] CacheError),
+
     #[error("Exec error: {0}")]
     ExecError(#[from] ExecError),
+
     #[error("Invalid target")]
     InvalidTarget,
+
     #[error("Route operation error: {0}")]
     RouteOpError(#[from] RouteOpError),
 }
 
 #[derive(Error, Debug)]
 pub enum CacheError {
-    #[error("")]
+    #[error("IO error: {0}")]
     IOError(#[from] std::io::Error),
 }
 
 #[derive(Error, Debug)]
 pub enum ExecError {
-    #[error("")]
+    #[error("Exec error: {0}")]
     ExecError(#[from] std::io::Error),
 }
 
-/// Error type for route table operation.
+/// Error type for route table operations.
 #[derive(Error, Debug)]
 pub enum RouteOpError {
-    #[error("Any IO Error")]
+    #[error("IO error: {0}")]
     OpError(#[from] std::io::Error),
+
     #[error("cannot find system default gateway")]
     NoGatewayError,
+
     #[error("cannot create handle")]
     HandleInitError,
+
     #[error("futures join error: {0}")]
     FutureError(#[from] tokio::task::JoinError),
-    #[error("Get default interface error: {0}")]
+
+    #[error("get default interface error: {0}")]
     GetInterfaceError(String),
-    #[error("Route Already exists")]
+
+    #[error("route already exists")]
     RouteAlreadyExistsError,
-    #[error("Route Not found")]
+
+    #[error("route not found")]
     RouteNotFoundError,
 }
 
